9a9ba3606f7060671bb7964deaacbc0f9139defe
Aaron Cox <aaronrcox123@gmail.com>
2023-12-03 12:40:45 +0800

    initial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Day1
+{
+    class CharMap
+    {
+        public int Index { get; set; }
+        public string Character { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "input.txt";
+            var lines = File.ReadAllLines(filename);
+            var lineDigits = lines.Select(line =>
+            {
+                var charMap = new List<CharMap>();
+                charMap.AddRange(AllIndexesOf(line, "one").Select(i => new CharMap() { Character = "1", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "1").Select(i => new CharMap() { Character = "1", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "two").Select(i => new CharMap() { Character = "2", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "2").Select(i => new CharMap() { Character = "2", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "three").Select(i => new CharMap() { Character = "3", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "3").Select(i => new CharMap() { Character = "3", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "four").Select(i => new CharMap() { Character = "4", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "4").Select(i => new CharMap() { Character = "4", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "five").Select(i => new CharMap() { Character = "5", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "5").Select(i => new CharMap() { Character = "5", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "six").Select(i => new CharMap() { Character = "6", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "6").Select(i => new CharMap() { Character = "6", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "seven").Select(i => new CharMap() { Character = "7", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "7").Select(i => new CharMap() { Character = "7", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "eight").Select(i => new CharMap() { Character = "8", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "8").Select(i => new CharMap() { Character = "8", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "nine").Select(i => new CharMap() { Character = "9", Index = i }));
+                charMap.AddRange(AllIndexesOf(line, "9").Select(i => new CharMap() { Character = "9", Index = i }));
+                return charMap.OrderBy(z => z.Index);
+            });
+
+
+            var lineNums = lineDigits.Select(line => int.Parse( line.First().Character.ToString() + line.Last().Character.ToString() ));
+            var total = lineNums.Sum();
+            Console.WriteLine(total);
+        }
+
+        static IEnumerable<int> AllIndexesOf(string str, string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                yield break;
+
+            int startIndex = 0;
+            while ((startIndex = str.IndexOf(value, startIndex, StringComparison.OrdinalIgnoreCase)) != -1)
+            {
+                yield return startIndex;
+                startIndex += value.Length; // Use startIndex++ to find overlapping matches
+            }
+        }
+    }
+}
---
+++ Day_02/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Day_02
+{
+    public class GameSet
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+
+        public int Power
+        {
+            get
+            {
+                return Red * Green * Blue;
+            }
+        }
+                
+    }
+
+    public class Game
+    {
+        public int Id { get; set; }
+        List<GameSet> GameSets { get; set; } = new List<GameSet>();
+
+
+
+        public Game(string input)
+        {
+            string[] parts = input.Split(':');
+            Id = int.Parse(parts[0].Replace("Game ", ""));
+            GameSets = parts[1].Split(";").Select(setStr =>
+            {
+                string[] setCount = setStr.Split(",");
+                int red = setCount.Where(z => z.EndsWith("red")).Select(z => z.Replace("red", "").Trim()).Select(int.Parse).Sum();
+                int green = setCount.Where(z => z.EndsWith("green")).Select(z => z.Replace("green", "").Trim()).Select(int.Parse).Sum();
+                int blue = setCount.Where(z => z.EndsWith("blue")).Select(z => z.Replace("blue", "").Trim()).Select(int.Parse).Sum();
+
+                return new GameSet()
+                {
+                    Red = red,
+                    Green = green,
+                    Blue = blue
+                };
+            }).ToList();
+        }
+
+        public int TotalRed { get { return GameSets.Select(z => z.Red).Sum(); } }
+        public int TotalGreen { get { return GameSets.Select(z => z.Green).Sum(); } }
+        public int TotalBlue { get { return GameSets.Select(z => z.Blue).Sum(); } }
+
+        public int MaxRed { get { return GameSets.Select(z => z.Red).Max(); } }
+        public int MaxGreen { get { return GameSets.Select(z => z.Green).Max(); } }
+        public int MaxBlue { get { return GameSets.Select(z => z.Blue).Max(); } }
+
+        public GameSet MinSet
+        { 
+            get 
+            {
+                return new GameSet()
+                {
+                    Red = MaxRed,
+                    Green = MaxGreen,
+                    Blue = MaxBlue,
+                };
+            }
+        }
+
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "input.txt";
+            var Games = File.ReadAllLines(filename).Select(z => new Game(z)).ToList();
+
+            var part1validGames = Games.Where(game => game.MaxRed <= 12 && game.MaxGreen <= 13 && game.MaxBlue <= 14).ToList();
+            var part1Answer = part1validGames.Select(z => z.Id).Sum();
+
+
+            var part2Answer = Games.Select(game => game.MinSet.Power).Sum();
+
+            Console.WriteLine(part1Answer);
+            Console.WriteLine(part2Answer);
+
+            }
+    }
+}
